2bcef52b1190d19391e6500c59e61c12fc07ad20
matheus-feliz <matheusfoliveira172@gmail.com>
2021-09-08 16:22:45 -0300

    project created

---
+++ backend/src/main/java/com/feliz/felizdsvendassds4/Felizdsvendassds4Application.java
@@ -0,0 +1,13 @@
+package com.feliz.felizdsvendassds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Felizdsvendassds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Felizdsvendassds4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/feliz/felizdsvendassds4/Felizdsvendassds4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feliz.felizdsvendassds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Felizdsvendassds4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
