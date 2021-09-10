b708330efe9d96156c82d68af1f5188929000d64
matheus-feliz <matheusfoliveira172@gmail.com>
2021-09-10 05:56:03 -0300

    Pagination

---
+++ backend/src/main/java/com/feliz/felizdsvendassds4/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.feliz.felizdsvendassds4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.feliz.felizdsvendassds4.entities.Sale;
+
+public interface SaleRepository  extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/feliz/felizdsvendassds4/service/SaleService.java
@@ -0,0 +1,32 @@
+package com.feliz.felizdsvendassds4.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.feliz.felizdsvendassds4.dto.SaleDTO;
+import com.feliz.felizdsvendassds4.entities.Sale;
+import com.feliz.felizdsvendassds4.repositories.SaleRepository;
+import com.feliz.felizdsvendassds4.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
