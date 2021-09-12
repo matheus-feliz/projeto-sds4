14510346905283600da35a17a1e46173d9520aa4
matheus-feliz <matheusfoliveira172@gmail.com>
2021-09-12 10:29:14 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -11,21 +12,19 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //forma errada
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // forma errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellername);
             const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries }
-            console.log(chartData);
+            setChartData({ labels: myLabels, series: mySeries});
         });
-
-
+    }, []);
+ 
     //const mockData = {
     //  series: [477138, 499928, 444867, 220426, 473088],
     //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
